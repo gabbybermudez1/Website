dcf217c28167b9380012d400305cab4800f1a27b
Gabby Bermudez <gabbybermudez1@gmail.com>
2020-07-07 15:55:03 -0400

    started again mobile-first. Completed Responsive header

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,32 +1,55 @@
-
-// wait for dom to load before executing the command cusror
-document.addEventListener("DOMContentLoaded", () => {
-    cursor = document.querySelector("#cmd-cursor");
-    setInterval(blink, 700);
-
-    desired_text = "Gabby Bermudez";
-    text = ""; 
-    counter = 0;
-    command = document.querySelector("#cmd-type");
-    setTimeout(type_effect, 2000);
-
-})
+var hamburger = document.querySelector(".mobile-menu");
+var nav_list = document.querySelector(".main-nav-list");
 
 
-const blink = () => {
-    if (cursor.style.display == 'none'){
-    cursor.style.display = ''
-    }
-    else{
-        cursor.style.display  = 'none'
-    }
+// Changes the toggles between showing the menu and not
+const toggle_menu_mobile = () => {
+    nav_list.style.display = (nav_list.style.display == "none" ? "block" : "none")
 }
 
-const type_effect = () => {
-    if ( counter < desired_text.length){
-        text = desired_text.substr(0, counter + 1);
-        command.innerHTML = text;
-        counter++;
-        setTimeout(type_effect, 100);
-    }
-}
+
+hamburger.addEventListener("click", toggle_menu_mobile);
+
+
+// // wait for dom to load before executing the command cusror
+// document.addEventListener("DOMContentLoaded", () => {
+//     cursor = document.querySelector("#cmd-cursor");
+//     setInterval(blink, 700);
+
+//     desired_text = "Gabby Bermudez";
+//     text = ""; 
+//     counter = 0;
+//     command = document.querySelector("#cmd-type");
+//     setTimeout(type_effect, 2000);
+
+// })
+
+// /**
+//  * Blink is the Function used  to make the cursor on the command line
+//  * flash every so often. 
+//  * 
+//  */
+// const blink = () => {
+//     if (cursor.style.display == 'none'){
+//     cursor.style.display = ''
+//     }
+//     else{
+//         cursor.style.display  = 'none'
+//     }
+// }
+
+
+// /**
+//  * type_effect is a function used to type out my name on the command
+//  * line.
+//  */
+// const type_effect = () => {
+//     if ( counter < desired_text.length){
+//         text = desired_text.substr(0, counter + 1);
+//         command.innerHTML = text;
+//         counter++;
+//         setTimeout(type_effect, 100);
+//     }
+// }
+
+
